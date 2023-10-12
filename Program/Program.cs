9c8fc95e61413ce249311737d7da7327581e17b3
Ács Zoltán Péter <b0acszol@vasvari.hu>
2023-10-12 09:16:07 +0200

    Hangman

---
+++ Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using Hang;
+
+
+var target = new Target();
+Console.WriteLine("Kérem a szót: ");
+
+target.SetWords(Console.ReadLine());
+
+var letter = new Target();
+Console.WriteLine("Adjon meg egy betűt: ");
+
+letter.CheckCharacter(string );
---
+++ Program/Target.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hang
+{
+    public class Target
+    {
+        public string Word;
+        public string HashedWord;
+
+        public void SetWords(string word)
+        {
+
+            Word = word;
+            for (int i = 0; i < word.Length; i++) ;
+            {
+                HashedWord = HashedWord + '*';
+            }
+        }
+
+        public void CheckCharacter(string characterForSearch)
+        { 
+        for string i = 
+        }
+
+    }
+}
